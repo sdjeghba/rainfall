62a83a44640f5e781a8399af2e1bf9aeb194f6e4
saremu <sdjeghba@student.42.fr>
2020-10-07 17:06:55 +0200

    level0 done

--- level0/source.c
+++ level0/source.c
@@ -4,6 +4,12 @@
 #include <sys/types.h>
 #include <stdio.h>
 
+/*
+**	We get the effetive suid et guid wich are level1's , and set them to our
+**	programm to open the shell. Don't forget that the /tmp setups of our VM
+**	disable suid, so we have to put the binary on level1 home.
+*/
+
 int	main(int ac, char **av)
 {
 	if (atoi(av[1]) == 423)
@@ -19,3 +25,4 @@ int	main(int ac, char **av)
 		fprintf(stderr, "No !\n");
 	return (0);
 }
+
