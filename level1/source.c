3dd23a02ab0e69e749f09daccdbba2cb5d9a04ff
saremu <sdjeghba@student.42.fr>
2020-10-13 19:00:13 +0200

    level1 complete

---
+++ level1/source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void	run(void)
+{
+	printf("Good... Wait what ?\n");
+	system("/bin/sh");
+}
+
+int		main(void)
+{
+	char buffer[76];
+
+	gets(buffer);
+	return(0);
+}
