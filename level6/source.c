aede1a390078daedc4caf88261a3a2c2472402fd
saremu <sdjeghba@student.42.fr>
2020-10-17 03:47:39 +0200

    level6 source file not finished

---
+++ level6/source.c
@@ -0,0 +1,17 @@
+void	n(void)
+{
+	system("/bin/cat /home/user/level7/.pass");
+	return (0);
+}
+
+void	m(void)
+{
+	puts("Nope");
+	return (0);
+}
+
+int		main(int ac, char **av)
+{
+	// a vouer 
+	return(0);
+}
