55ceaec985d6928c63fbc8f16e45179e3e2a9890
saremu <sdjeghba@student.42.fr>
2020-10-27 07:45:27 +0100

    doubarassek

--- level6/source.c
+++ level6/source.c
@@ -1,17 +1,21 @@
 void	n(void)
 {
 	system("/bin/cat /home/user/level7/.pass");
-	return (0);
 }
 
 void	m(void)
 {
 	puts("Nope");
-	return (0);
 }
 
 int		main(int ac, char **av)
 {
-	// a vouer 
+	char *str = malloc(64);
+	void *addr = malloc(4);
+	void (*ptr)(void);
+	addr = (char *)0x8048468;
+	strcpy(str, av[1]);
+	ptr = addr;
+	(*ptr)();
 	return(0);
 }
