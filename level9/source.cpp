23f5591271c69b76829065876e695d1a377d9206
saremu <sdjeghba@student.42.fr>
2020-10-27 15:27:59 +0100

    source added

---
+++ level9/source.cpp
@@ -0,0 +1,28 @@
+class	Number
+{
+	private:
+		int		number;
+		char	buffer[100];
+
+	public:
+		Number(int x) : number(x) {}
+		void setAnnotation(char *str) {
+			memcpy(buffer, str, strlen(str));
+		}
+		virtual int operator+(Number &otherCL){
+			return number + otherCL.number;
+		}
+}
+
+int		main(int ac, char **av)
+{
+	if (ac < 2)
+		exit(1);
+	Number *first = new Number(5);
+	Number *second = new Number(6);
+	Number &tmp1 = *first;
+	Number &tmp2 = *second;
+
+	tmp1.setAnnotation(av[1]);
+	return tmp1 + tmp2;
+}
